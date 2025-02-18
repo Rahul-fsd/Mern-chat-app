ef0a4be92ec01a610e55a93a66d4140f89486ca0
Rahul-fsd <rahulibsar@gmail.com>
2025-02-18 22:51:47 +0530

    changed the mongodb connection params

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,12 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }).then((conn) => {
+      console.log(`MongoDB Connected: ${conn.connection.host}.cyan.underline`);
+    })
   } catch (error) {
     console.error(`Error: ${error.message}`.red.bold);
     process.exit(1); // Exit with a non-zero status code to indicate an error
